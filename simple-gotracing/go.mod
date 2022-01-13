175dfc4a02289fc692160f727ff583f12097d524
yzhao1012 <yzhao@pixielabs.ai>
2022-01-13 14:33:18 -0800

    Revert simple-gotracing
    Signed-off-by: yzhao1012 <yzhao@pixielabs.ai>

--- simple-gotracing/go.mod
+++ simple-gotracing/go.mod
@@ -4,8 +4,5 @@ go 1.13
 
 require (
 	github.com/fatih/color v1.9.0
-	github.com/iovisor/gobpf v0.2.0
-	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
-	google.golang.org/grpc v1.43.0 // indirect
-	google.golang.org/protobuf v1.25.0 // indirect
+	github.com/iovisor/gobpf v0.0.0-20200614202714-e6b321d32103
 )
--- simple-gotracing/http_trace_uprobe/bpf_program.go
+++ simple-gotracing/http_trace_uprobe/bpf_program.go
@@ -5,20 +5,18 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package main
 
-// This does not work for Golang programs built with toolchain version 1.17 or newer. Go 1.17 uses a register-based
-// calling convention which the BPF code here cannot handle.
 const bpfProgram = `
 #include <uapi/linux/ptrace.h>
 /***********************************************************
